d4988b41a168c6c4767b906dcbb4eb044758ee1b
Ethan Chen <intervigil@gmail.com>
2014-11-10 14:34:48 -0800

    build: Enable QCOM_BSP, QC_AV with QCOM_HARDWARE
    Change-Id: Ibf9dd35272521109fea52e46bacf6e1e3074ed6a

--- core/qcom_target.mk
+++ core/qcom_target.mk
@@ -6,19 +6,19 @@ ifeq ($(BOARD_USES_QCOM_HARDWARE),true)
     TARGET_GLOBAL_CFLAGS += -DQCOM_HARDWARE
     TARGET_GLOBAL_CPPFLAGS += -DQCOM_HARDWARE
 
-    ifeq ($(TARGET_USES_QCOM_BSP),true)
-        TARGET_GLOBAL_CFLAGS += -DQCOM_BSP
-        TARGET_GLOBAL_CPPFLAGS += -DQCOM_BSP
-    endif
+    TARGET_USES_QCOM_BSP := true
+    TARGET_GLOBAL_CFLAGS += -DQCOM_BSP
+    TARGET_GLOBAL_CPPFLAGS += -DQCOM_BSP
+
+    TARGET_ENABLE_QC_AV_ENHANCEMENTS := true
 
     # Enable DirectTrack for legacy targets
-    ifneq ($(filter caf bfam,$(TARGET_QCOM_AUDIO_VARIANT)),)
-        ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
-            TARGET_GLOBAL_CFLAGS += -DQCOM_DIRECTTRACK
-            TARGET_GLOBAL_CPPFLAGS += -DQCOM_DIRECTTRACK
-        endif
+    ifneq ($(filter msm7x30 msm8660 msm8960,$(TARGET_BOARD_PLATFORM)),)
+    ifeq ($(BOARD_USES_LEGACY_ALSA_AUDIO),true)
+        TARGET_GLOBAL_CFLAGS += -DQCOM_DIRECTTRACK
+        TARGET_GLOBAL_CPPFLAGS += -DQCOM_DIRECTTRACK
     endif
-endif
+    endif
 
 # Populate the qcom hardware variants in the project pathmap.
 define qcom-set-path-variant
