680374d050d4e6045e4ce0e009c40c131994be5a
Joe Maples <frap129@gmail.com>
2015-11-26 21:56:50 -0500

    core: Add USE_GCC_PLATFORM_BUILD Option
    Signed-off-by: Joe Maples <frap129@gmail.com>

--- core/binary.mk
+++ core/binary.mk
@@ -185,6 +185,13 @@ ifeq ($(USE_CLANG_PLATFORM_BUILD),true)
     endif
 endif
 
+# Add option to make gcc the default for device build
+ifeq ($(USE_GCC_PLATFORM_BUILD),true)
+    ifeq ($(my_clang),true)
+        my_clang := 
+    endif
+endif
+
 # arch-specific static libraries go first so that generic ones can depend on them
 my_static_libraries := $(LOCAL_STATIC_LIBRARIES_$($(my_prefix)$(LOCAL_2ND_ARCH_VAR_PREFIX)ARCH)) $(LOCAL_STATIC_LIBRARIES_$(my_32_64_bit_suffix)) $(my_static_libraries)
 my_whole_static_libraries := $(LOCAL_WHOLE_STATIC_LIBRARIES_$($(my_prefix)$(LOCAL_2ND_ARCH_VAR_PREFIX)ARCH)) $(LOCAL_WHOLE_STATIC_LIBRARIES_$(my_32_64_bit_suffix)) $(my_whole_static_libraries)
